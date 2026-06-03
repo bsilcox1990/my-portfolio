99fa89e4bc18f2799e33c6913cc0d06e720d9f9e
Bradley Silcox <bradleysilcox80@gmail.com>
2026-06-03 12:31:25 -0700

    Add new project

--- src/app/constants/projects.ts
+++ src/app/constants/projects.ts
@@ -2,16 +2,24 @@ import type { Project } from "@/app/types/project"
 
 export const projects:Project [] = [
     {
+        title: "Job Tracker",
+        description: 'A full-stack job application tracking platform that allows users to securely manage their job search with authentication, personalized dashboards, and CRUD functionality.',
+        tech: ["React", "TypeScript", "Express", "PostgreSQL", "JWT", "Docker", "Neon", "Render", "Vercel"],
+        github: "https://github.com/bsilcox1990/job-tracker",
+        image: "/projects/jobTracker.png"
+    },
+
+    {
         title: "News Explorer",
         description: "An application used to search for relevant news and save articles to your profile",
-        tech: ["react", "vite", "css", "html", "javascript"],
+        tech: ["React", "Vite", "CSS", "HTML", "JavaScript"],
         github: "https://github.com/bsilcox1990/se_project_news-explorer",
         image: "/projects/newsExplorer.png"
     },
     {
         title: "What to wear(WTWR)",
         description: "A weather based clothing application that uses real time weather data to suggest articles of clothing from your profile",
-        tech: ["react", "vite", "css", "html", "javascript", "express", "node.js", "mongoDB"],
+        tech: ["React", "Vite", "CSS", "HTML", "JavaScript", "Express", "Node.js", "MongoDB"],
         repos: {
             frontend: "https://github.com/bsilcox1990/se_project_react",
             backend: "https://github.com/bsilcox1990/se_project_express"
@@ -21,7 +29,7 @@ export const projects:Project [] = [
     {
         title: "Spots",
         description: "A picture sharing application where users can add, delete, and like pictures",
-        tech: ["javascript", "css", "html", "webpack"],
+        tech: ["JavaScript", "CSS", "HTML", "Webpack"],
         github: "https://github.com/bsilcox1990/se_project_spots",
         image: "/projects/spots.png"
     }
